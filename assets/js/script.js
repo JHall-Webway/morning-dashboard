3c8f1fee7e32510c29c7d68177de0af1feaefc7e
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-07-24 13:51:19 -0700

    added city button, cleaned empty buttons off

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-  
+
 //Attempt to pull existing data from localStorage 
 var savedCity = localStorage.getItem("city");
 var userName = localStorage.getItem("name");
@@ -41,6 +41,8 @@ function modalMaker(type) {
             localStorage.setItem('name', name);
             localStorage.setItem('city', cityName);
             if (cityName && name) {
+                $("#city-button").show();
+                $("#username-button").show();
                 displayInfo(cityName, name);
                 getCityData(cityName, "startup");
                 $(".modal").hide();
@@ -71,6 +73,7 @@ function modalMaker(type) {
             var name = $("#nameInput").val().trim();
             localStorage.setItem('name', name);
             if (name) {
+                $("#username-button").show();
                 displayInfo(null, name);
                 $(".modal").hide();
             } else {
@@ -100,6 +103,7 @@ function modalMaker(type) {
             var cityName = $("#cityInput").val().trim();
             localStorage.setItem('city', cityName);
             if (cityName) {
+                $("#city-button").show();
                 displayInfo(cityName, null);
                 getCityData(cityName, "startup");
                 $(".modal").hide();
@@ -117,15 +121,6 @@ function modalMaker(type) {
                 .text("Error!"))
     }
 };
-
-//Displays user name and city in element
-function displayInfo(city, name) {
-    if (city && name) {
-        $(".userName").text(name);
-    } else if (!city && name) {
-        $(".userName").text(name);
-    }
-}
 
 //Fetches immediate data for chosen city, including coordinates and current weather.
 function getCityData(city, module) {
@@ -166,36 +161,82 @@ function getForecast(city) {
             }
         })
         .then(function (data) {
-            var iconCode = data.current.weather[0].icon;
-            var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
-            $('#weather')
-                .append($('<img>')
-                .attr("id", "current-icon")
-                .attr('src', iconURL));
-            $('#weather')
-                .append($('<h1>')
-                .text('Temperature: ' + data.current.temp + 'K'));
-            $('#weather')
-                .append($('<h1>')
-                .text('Wind: ' + data.current.wind_speed + 'MPH'));
-            $('#weather')
-                .append($('<h1>')
-                .text('Humidity: ' + data.current.humidity + '%'));
-            $('#weather')
-                .append($('<h1>')
-                .text('UV Index: ' + data.current.uvi));
+            console.log(data);
+            displayWeather(data);
+            // var iconCode = data.current.weather[0].icon;
+            // var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+            // $('#weather')
+            //     .append($('<img>')
+            //         .attr("id", "current-icon")
+            //         .attr('src', iconURL));
+            // $('#weather')
+            //     .append($('<h1>')
+            //         .text('Temperature: ' + Math.floor(((data.current.temp - 273.15) * 9 / 5 + 32)) + 'F'));
+            // $('#weather')
+            //     .append($('<h1>')
+            //         .text('Wind: ' + data.current.wind_speed + 'MPH'));
+            // $('#weather')
+            //     .append($('<h1>')
+            //         .text('Humidity: ' + data.current.humidity + '%'));
+            // $('#weather')
+            //     .append($('<h1>')
+            //         .text('UV Index: ' + data.current.uvi));
 
-            var currentTempH1 = document.querySelector('#dashboard-current-h1-temperature');
-            currentTempH1.textContent = 'Temperature: ' + data.current.temp + 'F';
-            var currentWindH1 = document.querySelector('#dashboard-current-h1-wind');
-            currentWindH1.textContent = 'Wind: ' + data.current.wind_speed + 'MPH';
-            var currentHumidityH1 = document.querySelector('#dashboard-current-h1-humidity');
-            currentHumidityH1.textContent = 'Humidity: ' + data.current.humidity + '%';
-            var currentUVH1 = document.querySelector('#dashboard-current-h1-uv');
-            currentUVH1.textContent = data.current.uvi;
-            var currentUVI = data.current.uvi;
+            // var currentTempH1 = document.querySelector('#dashboard-current-h1-temperature');
+            // currentTempH1.textContent = 'Temperature: ' + data.current.temp + 'F';
+            // var currentWindH1 = document.querySelector('#dashboard-current-h1-wind');
+            // currentWindH1.textContent = 'Wind: ' + data.current.wind_speed + 'MPH';
+            // var currentHumidityH1 = document.querySelector('#dashboard-current-h1-humidity');
+            // currentHumidityH1.textContent = 'Humidity: ' + data.current.humidity + '%';
+            // var currentUVH1 = document.querySelector('#dashboard-current-h1-uv');
+            // currentUVH1.textContent = data.current.uvi;
+            // var currentUVI = data.current.uvi;
 
         });
+}
+
+//Displays user name and city in element
+function displayInfo(city, name) {
+    if (city && name) {
+        $("#city-button").text(city);
+        $(".userName").text(name);
+    } else if (!city && name) {
+        $(".userName").text(name);
+    } else if (city && !name) {
+        $("#city-button").text(city);
+    }
+}
+
+function displayWeather(cityObj) {
+    $("#weather").empty();
+    var iconCode = cityObj.current.weather[0].icon;
+    var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    $('#weather')
+        .append($('<img>')
+            .attr("id", "current-icon")
+            .attr('src', iconURL));
+    $('#weather')
+        .append($('<h1>')
+            .text('Temperature: ' + Math.floor(((cityObj.current.temp - 273.15) * 9 / 5 + 32)) + 'F'));
+    $('#weather')
+        .append($('<h1>')
+            .text('Wind: ' + cityObj.current.wind_speed + 'MPH'));
+    $('#weather')
+        .append($('<h1>')
+            .text('Humidity: ' + cityObj.current.humidity + '%'));
+    $('#weather')
+        .append($('<h1>')
+            .text('UV Index: ' + cityObj.current.uvi));
+
+    var currentTempH1 = document.querySelector('#dashboard-current-h1-temperature');
+    currentTempH1.textContent = 'Temperature: ' + cityObj.current.temp + 'F';
+    var currentWindH1 = document.querySelector('#dashboard-current-h1-wind');
+    currentWindH1.textContent = 'Wind: ' + cityObj.current.wind_speed + 'MPH';
+    var currentHumidityH1 = document.querySelector('#dashboard-current-h1-humidity');
+    currentHumidityH1.textContent = 'Humidity: ' + cityObj.current.humidity + '%';
+    var currentUVH1 = document.querySelector('#dashboard-current-h1-uv');
+    currentUVH1.textContent = cityObj.current.uvi;
+    var currentUVI = cityObj.current.uvi;
 }
 
 //Uses coordiates to display google map with traffic overlay
@@ -215,18 +256,28 @@ if (savedCity && userName) {
 } else if (!savedCity && !userName) {
     savedCity = "";
     userName = "";
+    $("#username-button").hide();
+    $("#city-button").hide();
     modalMaker("both");
 } else if (!savedCity && userName) {
     savedCity = "";
+    $("#city-button").hide();
     displayInfo(null, userName);
     modalMaker("city");
 } else if (savedCity && !userName) {
     userName = "";
+    $("#username-button").hide();
+    displayInfo(savedCity, null);
     modalMaker("name");
     getCityData(savedCity, "startup");
 };
 
-$('#username-button').on('click', function(event) {
+$('#username-button').on('click', function (event) {
     event.preventDefault();
     modalMaker('both');
+});
+
+$("#city-button").on("click", function (event) {
+    event.preventDefault();
+    modalMaker("city");
 });
